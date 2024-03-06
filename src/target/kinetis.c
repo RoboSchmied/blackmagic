8cd6602831f2c490bdc73f5464e4ab9af978cce1
dragonmux <git@dragonmux.network>
2024-03-06 17:46:33 +0000

    target: Corrected the nomenclature and typing for `target_add_ram32()`

--- src/target/kinetis.c
+++ src/target/kinetis.c
@@ -146,8 +146,8 @@ static void kl_s32k14_setup(
 	target_s *const t, const uint32_t sram_l, const uint32_t sram_h, const size_t flash_size, const size_t flexmem_size)
 {
 	t->driver = "S32K14x";
-	target_add_ram(t, sram_l, 0x20000000U - sram_l);
-	target_add_ram(t, 0x20000000, sram_h);
+	target_add_ram32(t, sram_l, 0x20000000U - sram_l);
+	target_add_ram32(t, 0x20000000, sram_h);
 
 	kinetis_add_flash(t, 0x00000000, flash_size, 0x1000, K64_WRITE_LEN);   /* P-Flash, 4 KB Sectors */
 	kinetis_add_flash(t, 0x10000000, flexmem_size, 0x1000, K64_WRITE_LEN); /* FlexNVM, 4 KB Sectors */
@@ -163,20 +163,20 @@ bool kinetis_probe(target_s *const t)
 		/* sram memory size */
 		switch ((sdid >> 16U) & 0x0fU) {
 		case 0x03U: /* 4 KB */
-			target_add_ram(t, 0x1ffffc00, 0x0400);
-			target_add_ram(t, 0x20000000, 0x0c00);
+			target_add_ram32(t, 0x1ffffc00, 0x0400);
+			target_add_ram32(t, 0x20000000, 0x0c00);
 			break;
 		case 0x04U: /* 8 KB */
-			target_add_ram(t, 0x1ffff800, 0x0800);
-			target_add_ram(t, 0x20000000, 0x1800);
+			target_add_ram32(t, 0x1ffff800, 0x0800);
+			target_add_ram32(t, 0x20000000, 0x1800);
 			break;
 		case 0x05U: /* 16 KB */
-			target_add_ram(t, 0x1ffff000, 0x1000);
-			target_add_ram(t, 0x20000000, 0x3000);
+			target_add_ram32(t, 0x1ffff000, 0x1000);
+			target_add_ram32(t, 0x20000000, 0x3000);
 			break;
 		case 0x06U: /* 32 KB */
-			target_add_ram(t, 0x1fffe000, 0x2000);
-			target_add_ram(t, 0x20000000, 0x6000);
+			target_add_ram32(t, 0x1fffe000, 0x2000);
+			target_add_ram32(t, 0x20000000, 0x6000);
 			break;
 		default:
 			return false;
@@ -213,28 +213,28 @@ bool kinetis_probe(target_s *const t)
 
 	case 0x251U:
 		t->driver = "KL25";
-		target_add_ram(t, 0x1ffff000, 0x1000);
-		target_add_ram(t, 0x20000000, 0x3000);
+		target_add_ram32(t, 0x1ffff000, 0x1000);
+		target_add_ram32(t, 0x20000000, 0x3000);
 		kinetis_add_flash(t, 0x00000000, 0x20000, 0x400, KL_WRITE_LEN);
 		break;
 	case 0x231U:
 		t->driver = "KL27x128"; // MKL27 >=128kb
-		target_add_ram(t, 0x1fffe000, 0x2000);
-		target_add_ram(t, 0x20000000, 0x6000);
+		target_add_ram32(t, 0x1fffe000, 0x2000);
+		target_add_ram32(t, 0x20000000, 0x6000);
 		kinetis_add_flash(t, 0x00000000, 0x40000, 0x400, KL_WRITE_LEN);
 		break;
 	case 0x271U:
 		switch ((sdid >> 16U) & 0x0f) {
 		case 4:
 			t->driver = "KL27x32";
-			target_add_ram(t, 0x1ffff800, 0x0800);
-			target_add_ram(t, 0x20000000, 0x1800);
+			target_add_ram32(t, 0x1ffff800, 0x0800);
+			target_add_ram32(t, 0x20000000, 0x1800);
 			kinetis_add_flash(t, 0x00000000, 0x8000, 0x400, KL_WRITE_LEN);
 			break;
 		case 5:
 			t->driver = "KL27x64";
-			target_add_ram(t, 0x1ffff000, 0x1000);
-			target_add_ram(t, 0x20000000, 0x3000);
+			target_add_ram32(t, 0x1ffff000, 0x1000);
+			target_add_ram32(t, 0x20000000, 0x3000);
 			kinetis_add_flash(t, 0x00000000, 0x10000, 0x400, KL_WRITE_LEN);
 			break;
 		default:
@@ -245,20 +245,20 @@ bool kinetis_probe(target_s *const t)
 		switch ((sdid >> 16U) & 0x0fU) {
 		case 3U:
 			t->driver = "KL02x32";
-			target_add_ram(t, 0x1ffffc00, 0x400);
-			target_add_ram(t, 0x20000000, 0xc00);
+			target_add_ram32(t, 0x1ffffc00, 0x400);
+			target_add_ram32(t, 0x20000000, 0xc00);
 			kinetis_add_flash(t, 0x00000000, 0x7fff, 0x400, KL_WRITE_LEN);
 			break;
 		case 2U:
 			t->driver = "KL02x16";
-			target_add_ram(t, 0x1ffffe00, 0x200);
-			target_add_ram(t, 0x20000000, 0x600);
+			target_add_ram32(t, 0x1ffffe00, 0x200);
+			target_add_ram32(t, 0x20000000, 0x600);
 			kinetis_add_flash(t, 0x00000000, 0x3fff, 0x400, KL_WRITE_LEN);
 			break;
 		case 1U:
 			t->driver = "KL02x8";
-			target_add_ram(t, 0x1fffff00, 0x100);
-			target_add_ram(t, 0x20000000, 0x300);
+			target_add_ram32(t, 0x1fffff00, 0x100);
+			target_add_ram32(t, 0x20000000, 0x300);
 			kinetis_add_flash(t, 0x00000000, 0x1fff, 0x400, KL_WRITE_LEN);
 			break;
 		default:
@@ -267,14 +267,14 @@ bool kinetis_probe(target_s *const t)
 		break;
 	case 0x031U: /* KL03 family */
 		t->driver = "KL03";
-		target_add_ram(t, 0x1ffffe00, 0x200);
-		target_add_ram(t, 0x20000000, 0x600);
+		target_add_ram32(t, 0x1ffffe00, 0x200);
+		target_add_ram32(t, 0x20000000, 0x600);
 		kinetis_add_flash(t, 0, 0x8000, 0x400, KL_WRITE_LEN);
 		break;
 	case 0x220U: /* K22F family */
 		t->driver = "K22F";
-		target_add_ram(t, 0x1c000000, 0x4000000);
-		target_add_ram(t, 0x20000000, 0x100000);
+		target_add_ram32(t, 0x1c000000, 0x4000000);
+		target_add_ram32(t, 0x20000000, 0x100000);
 		kinetis_add_flash(t, 0, 0x40000, 0x800, KL_WRITE_LEN);
 		kinetis_add_flash(t, 0x40000, 0x40000, 0x800, KL_WRITE_LEN);
 		break;
@@ -285,8 +285,8 @@ bool kinetis_probe(target_s *const t)
 		 * subfamily nibble as 2
 		 */
 		t->driver = "K64";
-		target_add_ram(t, 0x1fff0000, 0x10000);
-		target_add_ram(t, 0x20000000, 0x30000);
+		target_add_ram32(t, 0x1fff0000, 0x10000);
+		target_add_ram32(t, 0x20000000, 0x30000);
 		kinetis_add_flash(t, 0, 0x80000, 0x1000, K64_WRITE_LEN);
 		kinetis_add_flash(t, 0x80000, 0x80000, 0x1000, K64_WRITE_LEN);
 		break;
@@ -303,22 +303,22 @@ bool kinetis_probe(target_s *const t)
 			/* K12 Sub-Family Reference Manual, K12P80M50SF4RM, Rev. 4, February 2013 */
 			case 0x7U:
 				t->driver = "MK12DX128Vxx5";
-				target_add_ram(t, 0x1fffc000, 0x00004000);                         /* SRAM_L, 16 KB */
-				target_add_ram(t, 0x20000000, 0x00004000);                         /* SRAM_H, 16 KB */
+				target_add_ram32(t, 0x1fffc000, 0x00004000);                       /* SRAM_L, 16 KB */
+				target_add_ram32(t, 0x20000000, 0x00004000);                       /* SRAM_H, 16 KB */
 				kinetis_add_flash(t, 0x00000000, 0x00020000, 0x800, KL_WRITE_LEN); /* P-Flash, 128 KB, 2 KB Sectors */
 				kinetis_add_flash(t, 0x10000000, 0x00010000, 0x800, KL_WRITE_LEN); /* FlexNVM, 64 KB, 2 KB Sectors */
 				break;
 			case 0x9U:
 				t->driver = "MK12DX256Vxx5";
-				target_add_ram(t, 0x1fffc000, 0x00004000);                         /* SRAM_L, 16 KB */
-				target_add_ram(t, 0x20000000, 0x00004000);                         /* SRAM_H, 16 KB */
+				target_add_ram32(t, 0x1fffc000, 0x00004000);                       /* SRAM_L, 16 KB */
+				target_add_ram32(t, 0x20000000, 0x00004000);                       /* SRAM_H, 16 KB */
 				kinetis_add_flash(t, 0x00000000, 0x00040000, 0x800, KL_WRITE_LEN); /* P-Flash, 256 KB, 2 KB Sectors */
 				kinetis_add_flash(t, 0x10000000, 0x00010000, 0x800, KL_WRITE_LEN); /* FlexNVM, 64 KB, 2 KB Sectors */
 				break;
 			case 0xbU:
 				t->driver = "MK12DN512Vxx5";
-				target_add_ram(t, 0x1fff8000, 0x00008000);                         /* SRAM_L, 32 KB */
-				target_add_ram(t, 0x20000000, 0x00008000);                         /* SRAM_H, 32 KB */
+				target_add_ram32(t, 0x1fff8000, 0x00008000);                       /* SRAM_L, 32 KB */
+				target_add_ram32(t, 0x20000000, 0x00008000);                       /* SRAM_H, 32 KB */
 				kinetis_add_flash(t, 0x00000000, 0x00040000, 0x800, KL_WRITE_LEN); /* P-Flash, 256 KB, 2 KB Sectors */
 				kinetis_add_flash(t, 0x00040000, 0x00040000, 0x800, KL_WRITE_LEN); /* FlexNVM, 256 KB, 2 KB Sectors */
 				break;
@@ -350,8 +350,8 @@ bool kinetis_probe(target_s *const t)
 		break;
 	case 0x118U: /* S32K118 */
 		t->driver = "S32K118";
-		target_add_ram(t, 0x1ffffc00, 0x00000400);                          /* SRAM_L, 1 KB */
-		target_add_ram(t, 0x20000000, 0x00005800);                          /* SRAM_H, 22 KB */
+		target_add_ram32(t, 0x1ffffc00, 0x00000400);                        /* SRAM_L, 1 KB */
+		target_add_ram32(t, 0x20000000, 0x00005800);                        /* SRAM_H, 22 KB */
 		kinetis_add_flash(t, 0x00000000, 0x00040000, 0x800, K64_WRITE_LEN); /* P-Flash, 256 KB, 2 KB Sectors */
 		kinetis_add_flash(t, 0x10000000, 0x00008000, 0x800, K64_WRITE_LEN); /* FlexNVM, 32 KB, 2 KB Sectors */
 		break;
